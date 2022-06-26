dbe4be52846bf72044dcf707358c17c5422e80bf
FloydLeHippie <104831656+FloydLeHippie@users.noreply.github.com>
2022-06-26 16:33:55 +0200

    v5

--- PAA2022_Script_NodeManager.sh
+++ PAA2022_Script_NodeManager.sh
@@ -16,14 +16,16 @@ sudo ssh-keygen
 #Copie de la clé publique ssh sur les hôtes ansible
 read -e -p "Entrez l'adresse ip du node 1" IP1
 sudo ssh-copy-id "ansible@${IP1}" -y
-#read -e -p "Entrez l'adresse ip du node 2" IP2
-#ssh-copy-id "ansible@${IP2}" -y
-#read -e -p "Entrez l'adresse ip du node 3" IP3
-#ssh-copy-id "ansible@${IP3}" -y
+read -e -p "Entrez l'adresse ip du node 2" IP2
+sudo ssh-copy-id "ansible@${IP2}" -y
+read -e -p "Entrez l'adresse ip du node 3" IP3
+sudo ssh-copy-id "ansible@${IP3}" -y
 
 #Créeation du fichier hosts
-sudo echo "$IP1\n$IP2\n$IP3" >> ./PAA2022/hosts.txt
+sudo echo "$IP1
+$IP2
+$IP3" >> ./PAA2022_hosts.txt
 
 sudo ansible-playbook -u ansible PAA2022_Playbook_Install_Docker.yml
-#ansible-playbook -u ansible PAA2022_Playbook_"".yml
-#ansible-playbook -u ansible PAA2022_Playbook_"".yml
+sudo ansible-playbook -u ansible PAA2022_Playbook_"".yml
+sudo ansible-playbook -u ansible PAA2022_Playbook_"".yml
