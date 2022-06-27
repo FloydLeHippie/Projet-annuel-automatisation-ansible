573788bbf3064739d8358881a723bba347eeaf10
FloydLeHippie <104831656+FloydLeHippie@users.noreply.github.com>
2022-06-28 01:06:18 +0200

    v12

--- PAA2022_Script_NodeManager.sh
+++ PAA2022_Script_NodeManager.sh
@@ -45,8 +45,8 @@ sudo ssh-copy-id ansible@$IP3
 #Créeation du fichier hosts
 sudo echo "Node1=$IP1
 Node2=$IP2
-Node3=$IP3" >> /home/Projet_Ansible/Playbooks/PAA2022_hosts
+Node3=$IP3" >> /home/Projet_Ansible/Playbooks/PAA2022_hosts.txt
 
-sudo ansible-playbook -i PAA2022_hosts -u ansible /home/Projet_Ansible/Playbooks/PAA2022_Playbook_Install_Docker.yml
-sudo ansible-playbook -i PAA2022_hosts -u ansible /home/Projet_Ansible/Playbooks/PAA2022_Playbook_PAA2022_Playbook_ZabbixAgent.yml
-sudo ansible-playbook -i PAA2022_hosts -u ansible /home/Projet_Ansible/Playbooks/PPA2022_Playbook_Deploiement_Serveur_Zabbix.yml
+sudo ansible-playbook -i PAA2022_hosts.txt -u ansible /home/Projet_Ansible/Playbooks/PAA2022_Playbook_Install_Docker.yml
+sudo ansible-playbook -i PAA2022_hosts.txt -u ansible /home/Projet_Ansible/Playbooks/PAA2022_Playbook_ZabbixAgent.yml
+sudo ansible-playbook -i PAA2022_hosts.txt -u ansible /home/Projet_Ansible/Playbooks/PPA2022_Playbook_Deploiement_Serveur_Zabbix.yml
