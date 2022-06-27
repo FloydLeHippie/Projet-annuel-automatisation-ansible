0b6368b63e6cac56c175da5877d9dd6616d39ca7
FloydLeHippie <104831656+FloydLeHippie@users.noreply.github.com>
2022-06-27 16:49:45 +0200

    v9

--- PAA2022_Script_NodeManager.sh
+++ PAA2022_Script_NodeManager.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$NicName=$(ls -I "lo" /sys/class/net)
+NicName=$(ls -I "lo" /sys/class/net)
 
 read -e -p "Entrez l'adresse ip :" IP
 read -e -p "Entrez le masque de sous réseau :" MASK
@@ -17,13 +17,13 @@ iface ${NicName}  inet static
  netmask ${MASK}
  gateway ${GW}
  dns-nameservers ${DNS}
-" | tee /etc/network/interfaces
+" | sudo tee /etc/network/interfaces
 
 #Commenter la source cdrom
 sudo sed -e '/cdrom/s/^/#/g' -i /etc/apt/sources.list
 
 #Rajouter la source bullseye-back-ports
-sudo echo "deb http://deb.debian.org/debian/ bullseye-backports main contrib" | sudo tee -a /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian/ bullseye-backports main contrib" | sudo tee -a /etc/apt/sources.list
 
 #Mise à jours de la liste des paquets
 sudo apt update
@@ -35,18 +35,18 @@ sudo apt install ansible -y
 sudo ssh-keygen
 
 #Copie de la clé publique ssh sur les hôtes ansible
-read -e -p "Entrez l'adresse ip du node 1" IP1
-sudo ssh-copy-id "ansible@${IP1}" -y
-read -e -p "Entrez l'adresse ip du node 2" IP2
-sudo ssh-copy-id "ansible@${IP2}" -y
-read -e -p "Entrez l'adresse ip du node 3" IP3
-sudo ssh-copy-id "ansible@${IP3}" -y
+read -e -p "Entrez l'adresse ip du node 1 :" IP1
+sudo ssh-copy-id ansible@${IP1} -y
+read -e -p "Entrez l'adresse ip du node 2 :" IP2
+sudo ssh-copy-id ansible@${IP2} -y
+read -e -p "Entrez l'adresse ip du node 3 :" IP3
+sudo ssh-copy-id ansible@${IP3} -y
 
 #Créeation du fichier hosts
 sudo echo "$IP1
 $IP2
 $IP3" >> ./PAA2022_hosts.txt
 
-sudo ansible-playbook -i hosts.txt -u ansible PAA2022_Playbook_Install_Docker.yml
-sudo ansible-playbook -u ansible PAA2022_Playbook_"".yml
-sudo ansible-playbook -u ansible PAA2022_Playbook_"".yml
+sudo ansible-playbook -i PAA2022_hosts -u ansible PAA2022_Playbook_Install_Docker.yml
+sudo ansible-playbook -i PAA2022_hosts -u ansible PAA2022_Playbook_PAA2022_Playbook_ZabbixAgent.yml
+sudo ansible-playbook -i PAA2022_hosts -u ansible PAA2022_Playbook_.yml
